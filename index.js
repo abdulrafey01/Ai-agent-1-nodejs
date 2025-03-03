9cef5879543928601046d4387bb848c3ef92dda7
Abdul Rafey <115981065+abdulrafey01@users.noreply.github.com>
2025-03-03 15:53:53 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+const chat = model.startChat({
+  history: [
+    {
+      role: "user",
+      parts: [{ text: "Hello" }],
+    },
+    {
+      role: "model",
+      parts: [{ text: "Great to meet you. What would you like to know?" }],
+    },
+  ],
+});
+
+let result = await chat.sendMessage("I have 2 dogs in my house.");
+console.log(result.response.text());
+let result2 = await chat.sendMessage("How many paws are in my house?");
+console.log(result2.response.text());
